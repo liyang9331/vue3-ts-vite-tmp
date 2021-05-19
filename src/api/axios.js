93626421bcfdaa28d554b2e564d7d1a970a3e7a6
liyang <389688501@qq.com>
2021-05-19 10:17:49 +0800

    修改package.json,增加 .env.[]环境变量

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,8 +4,19 @@ axios.defaults.timeout = 60000
 // 跨域请求，允许保存cookie
 axios.defaults.withCredentials = true
 axios.defaults.headers['channelNo'] = "WEB" // 让每个请求携带channelNo
-// 请求地址的域名
-axios.defaults.baseURL = "/service" // 启用代理，参见下方代理配置
+
+
+const IsProduction = process.env.NODE_ENV === "production";
+// 根据运行环境动态设置请求地址的域名
+if(IsProduction){
+  // 生产环境
+  axios.defaults.baseURL = "/"
+}else{
+  // 开发环境
+  axios.defaults.baseURL = "/api" // 启用代理，参见下方代理配置
+}
+
+
 // 设置 post、put 默认 Content-Type
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.headers.put['Content-Type'] = 'application/json'
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const IsProduction = process.env.NODE_ENV === "production";
 module.exports = {
     publicPath: './', //修改打包根路径
     // 配置webpack-dev-server
@@ -30,36 +31,42 @@ module.exports = {
     // 去除 .map 文件
     productionSourceMap: false,
     chainWebpack: config => {
-        // 图片压缩
-        config.plugins.delete('prefetch')
-        config.module.rule('images')
-            .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
-            .use('image-webpack-loader')
-            .loader('image-webpack-loader')
-            .options({ bypassOnDebug: true })
-        const imagesRule = config.module.rule("images")
-        // 将图片大小限制设置为 6kb，低于6kb的图片全部被内联，高于6kb的图片会放在单独的img文件夹中
-        imagesRule
-            .use('url-loader')
-            .loader('url-loader')
-            .tap(options => Object.assign(options, { limit: 6144 }))
+        if (IsProduction) {
+            // 图片压缩
+            config.plugins.delete('prefetch')
+            config.module.rule('images')
+                .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+                .use('image-webpack-loader')
+                .loader('image-webpack-loader')
+                .options({ bypassOnDebug: true })
+            const imagesRule = config.module.rule("images")
+            // 将图片大小限制设置为 6kb，低于6kb的图片全部被内联，高于6kb的图片会放在单独的img文件夹中
+            imagesRule
+                .use('url-loader')
+                .loader('url-loader')
+                .tap(options => Object.assign(options, { limit: 6144 }))
+        }
+
     },
     // 代码压缩
     configureWebpack: config => {
-        const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-        config.plugins.push(
-            new UglifyJsPlugin({
-                uglifyOptions: {
-                    //生产环境自动删除console
-                    compress: {
-                        drop_debugger: true,
-                        drop_console: true,
-                        pure_funcs: ['console.log']
-                    }
-                },
-                sourceMap: false,
-                parallel: true
-            })
-        )
+        if (IsProduction) {
+            const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+            config.plugins.push(
+                new UglifyJsPlugin({
+                    uglifyOptions: {
+                        //生产环境自动删除console
+                        compress: {
+                            drop_debugger: true,
+                            drop_console: true,
+                            pure_funcs: ['console.log']
+                        }
+                    },
+                    sourceMap: false,
+                    parallel: false
+                })
+            )
+        }
+
     }
 }
